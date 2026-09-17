e30cb0fdc0ec6bd89c3274046b9dc3d397a72abe
unitedmr046-dev <unitedmr046@gmail.com>
2026-09-17 14:50:08 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,5 +4,5 @@ pluginManagement {
 dependencyResolutionManagement {
     repositories { google(); mavenCentral() }
 }
-rootProject.name = "KasirRumahMakan"
+rootProject.name = "iyonzkasir"
 include(":app")
