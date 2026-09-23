0c4ff560f36e673fc804de88a93d0299e1aaad38
unitedmr046-dev <unitedmr046@gmail.com>
2026-09-23 12:02:19 +0800

    Update MainActivity.kt

--- app/src/main/java/com/iyonzkasir/MainActivity.kt
+++ app/src/main/java/com/iyonzkasir/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
